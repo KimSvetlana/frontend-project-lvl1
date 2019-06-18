6441aca78b254b5962c019b6e161f48db803387d
SvetaKim <svetlanamingalieva@gmail.com>
2019-06-18 08:35:02 +0700

    added brain-even

---
+++ src/bin/brain-even.js
@@ -0,0 +1,6 @@
+#!/bin/env node
+import {yourName,isEven} from "..";
+console.log('Welcome to the Brain Games!');
+
+let name = yourName();
+isEven(name);
--- src/index.js
+++ src/index.js
@@ -3,4 +3,34 @@ import readlineSync from 'readline-sync';
 export const yourName = () => {
 	const name = readlineSync.question('May I have your name?');
 	console.log(`Hello, ${name}!`);
+	return name;
+};
+
+export const isEven = (name) => {
+
+	const correctAnswer = (num) => {
+		if (num % 2 === 0) {
+			return 'yes';
+		}
+		else {
+			return 'no';
+		}
+	}
+	
+	
+	for (let i = 1; i <= 3; i++) {
+		const num1 = Math.round(Math.random() * 100);
+		console.log('Question: ' + num1);
+		const yourAnswer = readlineSync.question('Your answer:');
+	
+		if(correctAnswer(num1) === yourAnswer) {
+		    console.log('Correct!');
+		}
+		else{
+		    console.log(`${yourAnswer} is wrong answer ;(. Correct answer was ${correctAnswer(num1)} \nLet\'s try again, ${name}`);		    	   
+		    return;
+		}
+	}
+	
+	console.log(`Congratulations, ${name}!`);
 }
