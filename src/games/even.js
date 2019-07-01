bda515564d82b818ff231a0a4617a12a88b84f5d
SvetaKim <svetlanamingalieva@gmail.com>
2019-07-01 21:45:47 +0700

    Prime and even refactoring

--- src/games/even.js
+++ src/games/even.js
@@ -4,10 +4,8 @@ import playGame from '..';
 const isEven = num => num % 2 === 0;
 
 const getDataForRound = () => {
-  const question = `${randomNumberGenerator(0, 100)}`;
-
+  const question = randomNumberGenerator(0, 100);
   const correctAnswer = isEven(question) ? 'yes' : 'no';
-
   return {
     question,
     correctAnswer,
--- src/games/prime.js
+++ src/games/prime.js
@@ -4,7 +4,7 @@ import playGame from '..';
 
 const isPrime = (number) => {
   if (number === 2) {
-    return false;
+    return true;
   }
   if (number > 2) {
     for (let i = 2; i < number; i += 1) {
@@ -17,8 +17,7 @@ const isPrime = (number) => {
 };
 
 const getDataForRound = () => {
-  const question = `${randomNumberGenerator(2, 100)}`;
-
+  const question = randomNumberGenerator(2, 100);
   const correctAnswer = isPrime(question) ? 'yes' : 'no';
   return {
     question,
